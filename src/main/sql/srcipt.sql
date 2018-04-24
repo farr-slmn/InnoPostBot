3a32ead17fc48f2c137cec35cb779689844faf82
farr-slmn <farr.slmn@gmail.com>
2018-04-25 01:26:09 +0300

    SQL table initialisation script(updated)

--- src/main/sql/srcipt.sql
+++ src/main/sql/srcipt.sql
@@ -1,12 +1,11 @@
 -- Table: public.recepients
 
--- DROP TABLE public.recepients;
+-- DROP TABLE public.recepients CASCADE;
 
 CREATE TABLE public.recepients
 (
-    id integer NOT NULL,
-    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT recepients_pkey PRIMARY KEY (id)
+    uid integer NOT NULL,
+    CONSTRAINT recepients_pkey PRIMARY KEY (uid)
 )
 WITH (
     OIDS = FALSE
@@ -15,16 +14,15 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.recepients
     OWNER to postgres;
-	
+
 -- Table: public.users
 
--- DROP TABLE public.users;
+-- DROP  TABLE public.users CASCADE;
 
 CREATE TABLE public.users
 (
-    id integer NOT NULL,
-    name character varying(255) COLLATE pg_catalog."default",
-    CONSTRAINT users_pkey PRIMARY KEY (id)
+    subname character varying(255) NOT NULL COLLATE pg_catalog."default",
+    CONSTRAINT users_pkey PRIMARY KEY (subname)
 )
 WITH (
     OIDS = FALSE
@@ -33,22 +31,22 @@ TABLESPACE pg_default;
 
 ALTER TABLE public.users
     OWNER to postgres;
-	
+
 -- Table: public.subscriptions
 
--- DROP TABLE public.subscriptions;
+-- DROP TABLE public.subscriptions CASCADE;
 
 CREATE TABLE public.subscriptions
 (
     uid integer NOT NULL,
-    subid integer NOT NULL,
-    CONSTRAINT subscriptions_pkey PRIMARY KEY (subid, uid),
-    CONSTRAINT subid FOREIGN KEY (subid)
-        REFERENCES public.recepients (id) MATCH SIMPLE
+    subname character varying(255) NOT NULL COLLATE pg_catalog."default",
+    CONSTRAINT subscriptions_pkey PRIMARY KEY (subname, uid),
+    CONSTRAINT subname FOREIGN KEY (subname)
+        REFERENCES public.users (subname) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION,
     CONSTRAINT uid FOREIGN KEY (uid)
-        REFERENCES public.users (id) MATCH SIMPLE
+        REFERENCES public.recepients (uid) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
